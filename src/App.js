afe620e8fd991a90650fdc7e7e576858eb57c3fc
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-02-24 11:53:32 +0100

    Header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Game from './components/Game';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Game numberOfRows={6} numberOfCells={5} />
     </div>
   );
